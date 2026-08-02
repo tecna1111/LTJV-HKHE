37cde9a4846f228ee5553aa10da8340c4b9171ff
tecna <tectec135579@gmail.com>
2026-08-02 19:54:49 +0700

    Init project structure with Spring Boot BE and React Vite FE

---
+++ cosre-backend/src/main/java/com/cosre/cosre_backend/CosreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cosre.cosre_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosreBackendApplication.class, args);
+	}
+
+}
