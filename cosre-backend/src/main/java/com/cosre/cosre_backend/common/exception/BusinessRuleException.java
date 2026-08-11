1319ace0b0a79ec3e7af972656ea01fc2309de02
tecna <tectec135579@gmail.com>
2026-08-11 20:55:44 +0700

    feat(team): implement team management backend

---
+++ cosre-backend/src/main/java/com/cosre/cosre_backend/common/exception/BusinessRuleException.java
@@ -0,0 +1,7 @@
+package com.cosre.cosre_backend.common.exception;
+
+public class BusinessRuleException extends RuntimeException {
+    public BusinessRuleException(String message) {
+        super(message);
+    }
+}
